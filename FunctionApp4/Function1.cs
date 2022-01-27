5f0f1cc1213d12148f972f30c1068acee41a6242
haimnewclass <56435506+haimnewclass@users.noreply.github.com>
2022-01-27 12:49:04 +0200

    ssss

--- FunctionApp4/Function1.cs
+++ FunctionApp4/Function1.cs
@@ -22,7 +22,7 @@ namespace FunctionApp4
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");   
+            log.LogInformation("C# HTTP trigger function processed a request.");    //////////22222222222222222222222222222
             string name = req.Query["name"];
                
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
